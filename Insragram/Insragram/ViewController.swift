5ef1ec3dca188e781d99c0e5163fdf47c73c3dcf
Алена Карпова <alenapancenko00@gmail.com>
2022-10-18 14:44:47 +0300

    init

---
+++ Insragram/Insragram/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Insragram
+//
+//  Created by Алена Панченко on 18.10.2022.
+//
+
+import UIKit
+
+/// view
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
