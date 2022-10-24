3a993b6464e6a4f4d693226b0554b8f645c7092d
Алена Карпова <alenapancenko00@gmail.com>
2022-10-24 09:08:40 +0300

    no message

---
+++ Instagram/Insragram/TopicalCell/PublicationsCollectionViewCell.swift
@@ -0,0 +1,10 @@
+//
+//  TopicalCollectionViewCell.swift
+//  Insragram
+//
+//  Created by Алена Панченко on 23.10.2022.
+//
+
+import UIKit
+// Ячейка коллекции
+final class PublicationsCollectionViewCell: UICollectionViewCell {}
